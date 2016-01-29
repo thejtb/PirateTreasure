e3bfdbcd500d8193c6de8c917cad64c025a8855b
Michael Arciola <mra33@live.com>
2016-01-29 15:31:06 -0500

    Removed the lolol comment

--- app/src/main/java/com/example/codecaboose/piratetreasurehunt/MainActivity.java
+++ app/src/main/java/com/example/codecaboose/piratetreasurehunt/MainActivity.java
@@ -27,10 +27,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-    //lolol
-
-
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
