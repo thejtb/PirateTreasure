535f38ba44d53eaecc2465786cf84abe934a2d9e
jeeriff <thegreatwhiteboo@Yahoo.com>
2016-02-09 21:30:16 -0500

    General cleanup of activity names and removal of default floating action buttons

--- app/src/main/java/com/example/codecaboose/piratetreasurehunt/MainActivity.java
+++ app/src/main/java/com/example/codecaboose/piratetreasurehunt/MainActivity.java
@@ -2,10 +2,7 @@ package com.example.codecaboose.piratetreasurehunt;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,15 +13,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_main_screen);
 
         Button playButton = (Button) findViewById(R.id.button);
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent go_play = new Intent(MainActivity.this, PlayScreen.class);
+                Intent go_play = new Intent(MainActivity.this, PlayActivity.class);
                 startActivity(go_play);
 
             }
@@ -35,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         createButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent go_create = new Intent(MainActivity.this, CreateScreen.class);
+                Intent go_create = new Intent(MainActivity.this, CreateActivity.class);
                 startActivity(go_create);
 
             }
--- app/src/main/java/com/example/codecaboose/piratetreasurehunt/PlayActivity.java
+++ app/src/main/java/com/example/codecaboose/piratetreasurehunt/PlayActivity.java
@@ -7,12 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class CreateScreen extends AppCompatActivity {
+public class PlayActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_screen);
+        setContentView(R.layout.activity_play_screen);
 
     }
 
