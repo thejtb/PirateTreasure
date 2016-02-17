1efa331c78a47635c0cc98d75853102fd061a730
jeeriff <thegreatwhiteboo@Yahoo.com>
2016-02-16 22:27:33 -0500

    Added some buttons (that don't do anything yet) to the Settings screen.  Added a few notes on screens to plan out spacing of elements.

--- app/src/main/java/com/example/codecaboose/piratetreasurehunt/CreateActivity.java
+++ app/src/main/java/com/example/codecaboose/piratetreasurehunt/CreateActivity.java
@@ -1,11 +1,13 @@
 package com.example.codecaboose.piratetreasurehunt;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class CreateActivity extends AppCompatActivity {
 
@@ -16,4 +18,10 @@ public class CreateActivity extends AppCompatActivity {
 
     }
 
+    public void addClue(View view) {
+        Intent intent = new Intent(this, PlayActivity.class);
+        EditText clueAddition = (EditText) findViewById(R.id.editText);
+        
+
+    }
 }
--- app/src/main/java/com/example/codecaboose/piratetreasurehunt/PlayActivity.java
+++ app/src/main/java/com/example/codecaboose/piratetreasurehunt/PlayActivity.java
@@ -5,7 +5,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
+import android.widget.TextView;
 
 public class PlayActivity extends AppCompatActivity {
 
@@ -13,6 +15,10 @@ public class PlayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_screen);
+
+        /*TextView textList = (TextView) findViewById(R.id.scrollList);
+        textList.setMovementMethod(new ScrollingMovementMethod());*/
+
 
     }
 
