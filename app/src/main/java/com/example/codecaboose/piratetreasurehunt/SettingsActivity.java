8148f1b6fb13e27f238a5d98e8429dd7266752b1
jeeriff <thegreatwhiteboo@Yahoo.com>
2016-03-01 13:42:19 -0500

    Switched checkboxes on settings screen to mutually exclusive radio buttons

--- app/src/main/java/com/example/codecaboose/piratetreasurehunt/SettingsActivity.java
+++ app/src/main/java/com/example/codecaboose/piratetreasurehunt/SettingsActivity.java
@@ -2,14 +2,38 @@ package com.example.codecaboose.piratetreasurehunt;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RadioButton;
 
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings_screen);
+        setContentView(R.layout.activity_settings_screen);}
 
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.radioButton:
+                if (checked)
+                     //5 clues
+                     break;
+                case R.id.radioButton2:
+                    if (checked)
+                        //10 clues
+                       break;
+
+              case R.id.radioButton3:
+                 if (checked)
+                     //15 clues
+                        break;
+         }
+        }
     }
 
-}
+
